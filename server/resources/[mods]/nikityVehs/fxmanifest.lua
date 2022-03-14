157b0976648d68c8e76b1b735e28286ab7cf4bb5
Eric Nakamura <masaro.eric@gmail.com>
2022-03-14 01:03:14 +0000

    NikityVehs
    +Mods movidos para pasta principal das streams(Talvez resolva o problema com os mods dos carros)
    +Possível solução da raptor estar capotando

--- server/resources/[mods]/nikityVehs/fxmanifest.lua
+++ server/resources/[mods]/nikityVehs/fxmanifest.lua
@@ -9,11 +9,11 @@ files {
 	"audio/**/*"
 }
 
-data_file "HANDLING_FILE" "data/handling.meta"
-data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
-data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
-data_file "CARCOLS_FILE" "data/carcols.meta"
-data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+data_file "HANDLING_FILE" "data/handling*.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts*.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles*.meta"
+data_file "CARCOLS_FILE" "data/carcols*.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations*.meta"
 data_file "FIVEM_LOVES_YOU_341B23A2F0E0F131" "data/popgroups.ymt"
 
 data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
