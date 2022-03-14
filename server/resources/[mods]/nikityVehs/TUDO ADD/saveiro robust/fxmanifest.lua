f2cb315c16e0ece8a2bf2204b0d9fc82963e8405
CabelaoNakamura <hidekiarthur32@gmail.com>
2022-03-14 00:59:37 +0000

    add cars

---
+++ server/resources/[mods]/nikityVehs/TUDO ADD/saveiro robust/fxmanifest.lua
@@ -0,0 +1,19 @@
+client_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+fx_version 'bodacious'
+game 'gta5'
+
+files  {
+	'data/vehicles.meta',
+	'data/carvariations.meta',
+	'data/carcols.meta',
+	'data/handling.meta',
+	'data/vehiclelayouts.meta', 
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'              
+
