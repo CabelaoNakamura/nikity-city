e7cd444e5eb21f489d8915d7187f0e73ef252432
Eric Nakamura <masaro.eric@gmail.com>
2022-03-24 21:24:23 +0000

    NikityMaps changes

--- server/resources/[mods]/nikityMaps/fxmanifest.lua
+++ server/resources/[mods]/nikityMaps/fxmanifest.lua
@@ -8,14 +8,32 @@ files {
 	"stream/*/**",
 	"stream/*/*/**",
 	"stream/*/*/*/**",
+	"stream/minimap.gfx"
+	"stream/hud_reticle.gfx"
 	"interiorproxies.meta"
 }
 
 data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
-data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/v_int_16.ytyp"
-data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/v_int_49.ytyp"
-data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/creative_hotel.ytyp"
-data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/creative_middle.ytyp"
-data_file 'DLC_ITYP_REQUEST' 'stream/patoche_elevatorb1.ytyp'
-data_file'DLC_ITYP_REQUEST' 'stream/nikityGaragePaleto/**paleto_garage_ytyp.ytyp'
-data_file'DLC_ITYP_REQUEST' 'stream/nikityGaragePaleto/**paleto_garage_props.ytyp'
+data_file "DLC_ITYP_REQUEST" "stream/*/v_int_16.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/*/v_int_49.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/*/creative_hotel.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/*/creative_middle.ytyp"
+data_file 'DLC_ITYP_REQUEST' 'stream/*/patoche_elevatorb1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/paleto_garage_ytyp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/paleto_garage_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/ponte_arlison.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/forest_lods.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/denis3d_tunershop_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/denis3d_tunershop_int.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/denis3d_tuners_metadata_001.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/verpi_park_objects.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/int_genstore.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/pablito_eb_propy.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/pablito_eb.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/cubo.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/denis3d_hayes_auto_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/denis3d_hayes_auto_int.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/lr_supermod_garage_int.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/patoche_elevatorb1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/country_01_metadata_019_strm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*/country_01_metadata_027.ytyp'
